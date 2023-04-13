89030126f401f1acafeb9dbaee86e7f7bd200fa5
Mkrtychiyants <heisaloosershesadgmail@gmail.com>
2023-04-13 20:00:44 +0300

    second ed

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,24 +1,17 @@
 import json from './parser';
 import read from './reader';
+import GameSaving from './GameSaving';
 
 export default class GameSavingLoader {
-  // eslint-disable-next-line class-methods-use-this
-  constructor(id, created, userInfo) {
-    this.id = id;
-    this.created = created;
-    this.userInfo = userInfo;
-  }
-
   static load() {
     return read()
       .then((res) => json(res))
       .then((result) => {
         const saving = JSON.parse(result);
-        const saveObj = new GameSavingLoader(saving.id, saving.created, saving.userInfo);
+        const saveObj = new GameSaving(saving.id, saving.created, saving.userInfo);
         return saveObj;
       }).catch((error) => {
         throw new Error(error);
       });
   }
 }
-
--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,8 @@
 import GameSavingLoader from '../GameSavingLoader';
+import GameSaving from '../GameSaving';
 
 test('promise test', async (done) => {
   const data = await GameSavingLoader.load();
-  expect(data).toBeDefined();
+  expect(data).toBeInstanceOf(GameSaving);
   done();
 });
